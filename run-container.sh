fddc77f3adc43ebcea2f3e9b10818eac7d43366f
Thomas Mutter <Thomas.Mutter@swisslife.ch>
2021-04-08 08:54:39 +0200

    No proxy

--- run-container.sh
+++ run-container.sh
@@ -1,7 +1,5 @@
 docker run \
-    --env http_proxy="http://10.200.154.25:3128" \
-    --env https_proxy="http://10.200.154.25:3128" \
-    -p 9001:80 \
+    -p 9080:80 \
     --name gitserver
     -i -t
     gitserver
